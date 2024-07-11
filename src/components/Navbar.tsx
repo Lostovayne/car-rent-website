76ab8689be838203775646873a01bd8e17b4a1de
Ismytv <ismytv@gmail.com>
2024-07-11 00:39:47 -0500

    feat:add component-ui Input

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,40 @@
+import { Search, SlidersHorizontal } from 'lucide-react';
+import { Input } from './ui/input';
+
 export const Navbar = () => {
-  return <div className="bg-red-100 w-full" >Navbar</div>;
+	return (
+		<div className='flex justify-between items-center mx-7 py-3'>
+			<Logo />
+			<SearchArea />
+			<ProfillNotification />
+		</div>
+	)
 };
+
+const Logo = () => {
+	return (
+		<span>MORENT</span>
+	)
+}
+
+const SearchArea = () => {
+	return (
+		<div className='flex border-2 items-center px-3 rounded-xl gap-3'>
+			<Search width={24} height={24} />
+
+			<Input
+				type='text'
+				placeholder='Search something here'
+				className='w-[24rem] border-0 focus:outline-none focus:outline-0'
+			/>
+
+			<SlidersHorizontal width={24} height={24} />
+		</div>
+	)
+}
+
+const ProfillNotification = () => {
+	return (
+		<span>Profill Notification</span>
+	)
+}
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,5 @@
 export * from "./Footer";
 export * from "./Navbar";
+
 export * from "./ui/button"
+export * from "./ui/input"
