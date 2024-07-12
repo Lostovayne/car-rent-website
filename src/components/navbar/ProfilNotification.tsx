b7656899671371592bcd5cfd2b242f7021d3fe8c
LostoVayne <moises.barrera.dev@gmail.com>
2024-07-12 19:38:05 -0400

    fixed elements

--- src/components/navbar/ProfilNotification.tsx
+++ src/components/navbar/ProfilNotification.tsx
@@ -1,56 +1,44 @@
-import { Bell, Heart, Settings } from 'lucide-react';
+import { Bell, Heart, Settings } from "lucide-react";
 
-import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar';
-import { Button } from '../ui/button';
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+import { Button } from "../ui/button";
 
 interface Props {
-    unreadNotifications: boolean
+  unreadNotifications: boolean;
 }
 
 export const ProfilNotification = ({ unreadNotifications }: Props) => {
-    return (
-        <div className='flex gap-4'>
-            <div className='sm:flex gap-4 hidden'>
-                {/* like */}
-                <Button
-                    variant="link"
-                    size="icon"
-                    className="border rounded-full"
-                >
-                    <Heart className="h-4 w-4 text-red-500 fill-red-500" />
-                </Button>
+  return (
+    <div className="flex gap-4">
+      <div className="sm:flex gap-4 hidden">
+        {/* like */}
+        <Button variant="link" size="icon" className="border rounded-full">
+          <Heart className="h-4 w-4 text-red-500 fill-red-500" />
+        </Button>
 
-                {/* notification */}
-                <Button
-                    variant="link"
-                    size="icon"
-                    className="border rounded-full relative"
-                >
-                    <Bell className="h-4 w-4 " />
-                    {
-                        unreadNotifications &&
-                        <div className='w-3 h-3 absolute bg-red-500 rounded-full top-[-2px] right-[-2px]'></div>
-                    }
-                </Button>
+        {/* notification */}
+        <Button
+          variant="link"
+          size="icon"
+          className="border rounded-full relative"
+        >
+          <Bell className="h-4 w-4 " />
+          {unreadNotifications && (
+            <div className="w-3 h-3 absolute bg-red-500 rounded-full top-[-2px] right-[-2px]"></div>
+          )}
+        </Button>
 
-                {/* settings */}
-                <Button
-                    variant="link"
-                    size="icon"
-                    className="border rounded-full"
-                >
-                    <Settings className="h-4 w-4" />
-                </Button>
-            </div>
+        {/* settings */}
+        <Button variant="link" size="icon" className="border rounded-full">
+          <Settings className="h-4 w-4" />
+        </Button>
+      </div>
 
-            {/* avatar */}
-            <Avatar>
-                <AvatarImage src="https://github.com/shadcn.png" className='' />
-                <AvatarFallback>CN</AvatarFallback>
-            </Avatar>
-
-
-
-        </div>
-    )
-}
+      {/* avatar */}
+      <Avatar>
+        <AvatarImage src="https://github.com/shadcn.png" className="" />
+        <AvatarFallback>CN</AvatarFallback>
+      </Avatar>
+    </div>
+  );
+};
