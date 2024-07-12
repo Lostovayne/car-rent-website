f6828ae93a223fca85d656f8418ce005c6224ea8
Ismytv <ismytv@gmail.com>
2024-07-12 11:10:11 -0500

    feat:add Props Navbar; add IUser

--- src/components/navbar/ProfileNotification.tsx
+++ src/components/navbar/ProfileNotification.tsx
@@ -2,12 +2,13 @@ import { Bell, Heart, Settings } from 'lucide-react';
 
 import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar';
 import { Button } from '../ui/button';
+import type { IUser } from '../../interfaces/user.interface';
 
 interface Props {
-    unreadNotifications: boolean
+    user: IUser
 }
 
-export const ProfilNotification = ({ unreadNotifications }: Props) => {
+export const ProfileNotification = ({ user }: Props) => {
     return (
         <div className='flex gap-4'>
             <div className='sm:flex gap-4 hidden'>
@@ -17,7 +18,7 @@ export const ProfilNotification = ({ unreadNotifications }: Props) => {
                     size="icon"
                     className="border rounded-full"
                 >
-                    <Heart className="h-4 w-4 text-red-500 fill-red-500" />
+                    <Heart className="size-4 text-red-500 fill-red-500" />
                 </Button>
 
                 {/* notification */}
@@ -26,10 +27,10 @@ export const ProfilNotification = ({ unreadNotifications }: Props) => {
                     size="icon"
                     className="border rounded-full relative"
                 >
-                    <Bell className="h-4 w-4 " />
+                    <Bell className="size-4 " />
                     {
-                        unreadNotifications &&
-                        <div className='w-3 h-3 absolute bg-red-500 rounded-full top-[-2px] right-[-2px]'></div>
+                        user.unreadNotifications &&
+                        <div className='size-3 absolute bg-red-400 rounded-full top-[-2px] right-[-2px]'></div>
                     }
                 </Button>
 
@@ -39,13 +40,13 @@ export const ProfilNotification = ({ unreadNotifications }: Props) => {
                     size="icon"
                     className="border rounded-full"
                 >
-                    <Settings className="h-4 w-4" />
+                    <Settings className="size-4" />
                 </Button>
             </div>
 
             {/* avatar */}
             <Avatar>
-                <AvatarImage src="https://github.com/shadcn.png" className='' />
+                <AvatarImage src={user.imageUrl} className='' />
                 <AvatarFallback>CN</AvatarFallback>
             </Avatar>
 
