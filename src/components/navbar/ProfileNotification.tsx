fdec85b24987f2fc263745058f7bb29d3f6dbac5
LostoVayne <moises.barrera.dev@gmail.com>
2024-07-12 20:00:41 -0400

    agrega cambios en el navbar

--- src/components/navbar/ProfileNotification.tsx
+++ src/components/navbar/ProfileNotification.tsx
@@ -2,18 +2,19 @@ import { Bell, Heart, Settings } from "lucide-react";
 
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 import { Button } from "../ui/button";
+import type { IUser } from "../../interfaces/user.interface";
 
 interface Props {
-  unreadNotifications: boolean;
+  user: IUser;
 }
 
-export const ProfilNotification = ({ unreadNotifications }: Props) => {
+export const ProfileNotification = ({ user }: Props) => {
   return (
     <div className="flex gap-4">
       <div className="sm:flex gap-4 hidden">
         {/* like */}
         <Button variant="link" size="icon" className="border rounded-full">
-          <Heart className="h-4 w-4 text-red-500 fill-red-500" />
+          <Heart className="size-4 text-red-500 fill-red-500" />
         </Button>
 
         {/* notification */}
@@ -22,21 +23,21 @@ export const ProfilNotification = ({ unreadNotifications }: Props) => {
           size="icon"
           className="border rounded-full relative"
         >
-          <Bell className="h-4 w-4 " />
-          {unreadNotifications && (
-            <div className="w-3 h-3 absolute bg-red-500 rounded-full top-[-2px] right-[-2px]"></div>
+          <Bell className="size-4 " />
+          {user.unreadNotifications && (
+            <div className="size-3 absolute bg-red-400 rounded-full top-[-2px] right-[-2px]"></div>
           )}
         </Button>
 
         {/* settings */}
         <Button variant="link" size="icon" className="border rounded-full">
-          <Settings className="h-4 w-4" />
+          <Settings className="size-4" />
         </Button>
       </div>
 
       {/* avatar */}
       <Avatar>
-        <AvatarImage src="https://github.com/shadcn.png" className="" />
+        <AvatarImage src={user.imageUrl} className="" />
         <AvatarFallback>CN</AvatarFallback>
       </Avatar>
     </div>
