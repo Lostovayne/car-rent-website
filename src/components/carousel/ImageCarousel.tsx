e30fea207732022d8b8dd1669eabcd36eee90d5d
casadjes <esmilcasado901@gmail.com>
2024-07-24 16:25:43 -0400

    perf: :art: carousel responsive terminado
    reemplace las etiquetas de shadcn por etiquetas html semanticas ya que no se usaban

--- src/components/carousel/ImageCarousel.tsx
+++ src/components/carousel/ImageCarousel.tsx
@@ -1,38 +1,30 @@
 import { useSelected } from "../../hooks/useSelected";
 import { ImagesCarouselProps } from "../../interfaces";
 import { cn } from "../../lib/utils";
-import { Carousel, CarouselItem } from "../ui/carousel";
 import { Image } from "./Image";
 
 export const ImageCarousel: React.FC<ImagesCarouselProps> = ({ images }) => {
   const { selectedImage, handleImageClick } = useSelected({ images });
 
   return (
-    <div className="flex flex-col gap-y-2 w-full h-full max-w-md">
-      <Carousel className="w-full h-64 bg-[url('/img/carouselBackground.png')] bg-cover bg-no-repeat rounded-lg overflow-hidden aspect-square">
-        <CarouselItem className="w-full h-full pl-0 ">
-          <Image src={selectedImage} alt="" />
-        </CarouselItem>
-      </Carousel>
+    <div className="flex flex-col gap-y-2 w-full h-full max-w-[500px] max-h-[500px]">
+      <header className="w-full aspect-square bg-[url('/img/carouselBackground.png')] bg-cover bg-no-repeat rounded-lg overflow-hidden ">
+        <Image src={selectedImage} alt="" />
+      </header>
 
-      <section className="w-full ">
-        <div
-          className=" flex flex-row aspect-square size-36 w-full justify-between
-         "
-        >
-          {images.map(({ id, url, alt }) => (
-            <div
-              key={id}
-              className={cn` cursor-pointer aspect-square transition-all border-2 border-transparent  ${
-                selectedImage === url &&
-                "scale-95 p-1 border-2 border-primaryColor/60  rounded-lg"
-              }`}
-              onClick={() => handleImageClick(id)}
-            >
-              <Image src={url} className="rounded-lg" alt={alt} />
-            </div>
-          ))}
-        </div>
+      <section className="w-full flex">
+        {images.map(({ id, url, alt }) => (
+          <div
+            key={id}
+            className={cn` w-1/3 aspect-square cursor-pointer transition-all border-2 border-transparent  ${
+              selectedImage === url &&
+              "scale-95 p-1 border-2 border-primaryColor/60 rounded-lg"
+            }`}
+            onClick={() => handleImageClick(id)}
+          >
+            <Image src={url} className="rounded-lg" alt={alt} />
+          </div>
+        ))}
       </section>
     </div>
   );
