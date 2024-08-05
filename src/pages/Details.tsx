f90334717c7f88276e7b483cad586380af4ed3de
casadjes <esmilcasado901@gmail.com>
2024-08-05 00:20:24 -0400

    refactor: :art: cambio en la entructura del codigo y refactorizacion, cambio a algunos nombres de interfaces mal escritos , actualizacion de importaciones en el index de las interfaces

--- src/pages/Details.tsx
+++ src/pages/Details.tsx
@@ -1,10 +1,31 @@
-import { CarDetails } from "../components"
-import { dataCarDetails } from "../data"
+import { ImageCarousel } from "../components/carousel/ImageCarousel";
+import { Reviews } from "../components/details/reviews/Reviews";
+import { dataCarDetails as carDetails, imagesUrl } from "../data";
 
 export const DetailsPage = () => {
-  const carDetails = dataCarDetails
-
   return (
-    <CarDetails carDetails={carDetails}/>
-  )
-}
+    <main className="flex bg-backgroundSecondary">
+      {/* right side : details*/}
+      <section className="flex flex-col w-full ">
+        {/* view car && detail car */}
+        <div className="flex flex-col md:flex-row w-full ">
+          <div className="flex items-center justify-center w-full p-1 md:w-1/2 ">
+            <ImageCarousel images={imagesUrl} />
+          </div>
+          <div className="w-full md:w-1/2 bg-indigo-300">Detail Car</div>
+        </div>
+
+        {/* reviews */}
+        <Reviews reviews={carDetails.reviews ?? []} />
+
+        <section>
+          {/* recent car */}
+          <div className="bg-blue-300">Recent Car</div>
+
+          {/* recomendationcar */}
+          <div className="bg-orange-300">Recomendation Car</div>
+        </section>
+      </section>
+    </main>
+  );
+};
