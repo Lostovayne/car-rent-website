2e226d166af6d7ff5c53e519485e4fd45643df55
Ismytv <ismytv@gmail.com>
2024-08-08 12:19:15 -0500

    refactor(reviews)

--- src/components/details/reviews/ButtonShowAllReviews.tsx
+++ src/components/details/reviews/ButtonShowAllReviews.tsx
@@ -1,26 +1,25 @@
 import { ChevronDown, ChevronUp } from "lucide-react";
 import { Button } from "../../ui/button";
 
-interface Props {
+
+interface ButtonShowAllReviewsProps {
   isShowAllReviews: boolean;
-  handleOnClick: () => void;
+  onClick: () => void;
 }
 
-export const ButtonShowAllReviews = ({
-  isShowAllReviews,
-  handleOnClick,
-}: Props) => {
+export const ButtonShowAllReviews: React.FC<ButtonShowAllReviewsProps> = ({
+  isShowAllReviews, onClick }) => {
   return (
     <div className="flex justify-center">
-      <Button variant={"ghost"} className="w-32 p-2" onClick={handleOnClick}>
+      <Button variant={"ghost"} className="w-32 p-2" onClick={onClick}>
         <span className="font-normal text-base text-textSecondary">
           {isShowAllReviews ? "Show less" : "Show All"}
         </span>
-        {isShowAllReviews ? (
-          <ChevronUp className="ml-2 size-4 text-textSecondary" />
-        ) : (
-          <ChevronDown className="ml-2 size-4 text-textSecondary" />
-        )}
+        {
+          isShowAllReviews
+            ? <ChevronUp className="ml-2 size-4 text-textSecondary" />
+            : <ChevronDown className="ml-2 size-4 text-textSecondary" />
+        }
       </Button>
     </div>
   );
