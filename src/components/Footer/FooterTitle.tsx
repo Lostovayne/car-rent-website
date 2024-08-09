cec47fdd504cbda7f778ac0746c3f838d0c4c173
casadjes <esmilcasado901@gmail.com>
2024-08-09 02:52:32 -0400

    refactor: :recycle: componetizando footer y cambios simples en AutoCard

---
+++ src/components/Footer/FooterTitle.tsx
@@ -0,0 +1,11 @@
+export const FooterTitle = () => {
+  return (
+    <header className="md:w-[292px]">
+      <h3 className="text-primaryColor font-bold text-3xl pb-4">MORENT</h3>
+      <p className="max-md:w-3/4 text-textSecondary ">
+        Our visions is to provide convenience and help increase your sales
+        business
+      </p>
+    </header>
+  );
+};
