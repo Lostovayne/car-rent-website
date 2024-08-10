26ae54ed8c5d0e28cea31187c37c1c2bfdd87ee3
casadjes <esmilcasado901@gmail.com>
2024-08-10 18:32:43 -0400

    refactor: :lipstick: cambios en la UI de la cardDetailsCard

--- src/components/details/CarDetailsCard/TableContainer.tsx
+++ src/components/details/CarDetailsCard/TableContainer.tsx
@@ -1,6 +1,6 @@
 export const TableContainer = ({ children }: { children: React.ReactNode }) => {
   return (
-    <tbody className="flex flex-col gap-2 max-[390px]:w-full w-1/2">
+    <tbody className="flex flex-col gap-2 max-[390px]:w-full w-1/2 min-[1060px]:w-full">
       {children}
     </tbody>
   );
