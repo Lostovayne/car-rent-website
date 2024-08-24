6390c3a74f5e685eaa8429697becd5fe5b10fc1c
casadjes <esmilcasado901@gmail.com>
2024-08-24 16:04:51 -0400

    refactor: :bug: cambio en la estructura de Category, aun hay que resolver lo de las keys

--- src/pages/Category.tsx
+++ src/pages/Category.tsx
@@ -1,15 +1,10 @@
-import { TbArrowsDownUp } from "react-icons/tb";
-import { SelectDate, Button } from "@/components";
+import { SelectDate } from "@/components";
 
 export const Category = () => {
   return (
     <div className="flex items-center justify-center ">
-      <div className="flex flex-col xl:flex-row justify-between items-center w-full px-8">
-        <SelectDate title="Pick Up" />
-        <Button>
-          <TbArrowsDownUp className="" />
-        </Button>
-        <SelectDate title="Drop Off" />
+      <div className="flex flex-col xl:flex-row justify-between items-center w-full ">
+        <SelectDate />
       </div>
     </div>
   );
