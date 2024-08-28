9fcf402cbedfa1af0f8a89cbf8851994e4d64307
LostoVayne <moises.barrera.dev@gmail.com>
2024-08-28 17:23:09 -0400

    agrega mejora del card

--- src/components/sectionCar/ViewCars.tsx
+++ src/components/sectionCar/ViewCars.tsx
@@ -1,6 +1,6 @@
 import { ArrayCars } from "../../data";
 import { cn } from "../../lib/utils";
-import { AutoCard } from "../autoCard/AutoCard";
+import { AutoCard } from "../autoCard/VehicleCard";
 
 type AutoCardProps = {
   cuantity: number;
